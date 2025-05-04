58acfe3363cdb3d56f5841a0f94ee7867431242e
Preet Chauhan <chauhanpreet271105@gmail.com>
2025-05-04 13:20:41 +0530

    Add files via upload

---
+++ Array methods l4.js
@@ -0,0 +1,31 @@
+// let fooditems = ["apple", "banana", "tomato", "cheese"];
+
+// fooditems.push("chips");
+// let deletedfooditems = fooditems.pop("cheese");
+// console.log(fooditems);
+// console.log("deleted item =",deletedfooditems);
+// console.log(fooditems.toString() );
+
+// let marks=[97,55,23,54,32];
+// let marvel_heroes=['thorn',"batman","ironman"];
+
+//  let concaneted_array =fooditems.concat(marks,marvel_heroes);
+//  console.log(concaneted_array);
+
+//  let marvel_heroes=['thor',"batman","ironman","shaktiman","krish"];
+//  let val = marvel_heroes.shift();
+//  console.log(val);
+//  console.log(marvel_heroes.slice(1));
+// marvel_heroes.splice(1,2,"Preet","chauhan");
+
+// console.log(marvel_heroes.splice(1,2,"Preet","chauhan"));
+// console.log(marvel_hero);
+
+// practice question
+let companies = ["Bloomberg","Microsoft","Uber","IBM","Netflix"];
+
+let arr = companies.shift();
+companies.splice(1,1,"Ola");
+// companies.pop("Amazon");
+
+console.log(companies);
