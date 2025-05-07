9669f279246a8c760d0bff1d49423339df65b318
Preet Chauhan <chauhanpreet271105@gmail.com>
2025-05-07 18:34:27 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,18 @@
+let str = "PREET CHAUHAN";
+console.log(str[9]);
+
+// template literals
+let specialstring = `This Is My Rule ${1+22+2}`;
+console.log(specialstring);
+
+let object={
+    item:"pen",
+    price:10,
+
+
+};
+let output = `The cost of ${object.item} is ${object.price} rupess`;
+console.log(output);
+// or
+
+// console.log("the cost of pen",object.item,"is",object.price,"rupees")
