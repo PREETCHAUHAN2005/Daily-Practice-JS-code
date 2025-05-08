de96f31807702f6a02924ca842507ecbe6c9bb6c
Preet Chauhan <chauhanpreet271105@gmail.com>
2025-05-08 11:50:28 +0530

    Add files via upload

---
+++ practice lec 7.js
@@ -0,0 +1,12 @@
+// let newBtn = document.createElement("button");
+
+// newBtn.innerText = "click me";
+// newBtn.style.backgroundColor = "red";
+// newBtn.style.color = "white";
+// console.log(newBtn);
+
+// document.querySelector("body").prepend(newBtn);
+// qs 2
+
+let para = document.querySelector("p");
+
